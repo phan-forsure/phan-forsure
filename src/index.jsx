c256f5ebbc46a5bb2f4aff891c9e95b7c3d5acdb
phan-forsure <phantommy38@gmail.com>
2024-05-22 23:32:15 +0200

    deployed website again (fixed 7: refresh 404)

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { HashRouter as Router}  from 'react-router-dom'
+import { BrowserRouter as Router}  from 'react-router-dom'
 import App from './components/App'
 
 const rootElement = ReactDOM.createRoot(document.getElementById('root'))  
 rootElement.render(
-    <Router>
+    <Router basename={`/${process.env.PUBLIC_URL}`}>
         <App />
     </Router>
 )
