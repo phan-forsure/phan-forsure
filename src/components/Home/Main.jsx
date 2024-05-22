c16c0d51f6a8c8338da868d6c5dde33a9182e7cc
phan-forsure <phantommy38@gmail.com>
2024-05-23 01:39:58 +0200

    finalize project v2 of my website (again fixed a button)

--- src/components/Home/Main.jsx
+++ src/components/Home/Main.jsx
@@ -10,7 +10,7 @@ const Main = () => {
                     <h1>اطلب ما تشتهى من <span>طعام بضغطه واحدة</span> </h1>
                     <p>هنا في موقع دلع كرشك نوفر لك تجربة لا مثيل لها</p>
                 </div>
-                <Link to={'/phan-forsure/Products'}>اطلب الان</Link>    
+                <Link to={'/Products'}>اطلب الان</Link>    
             </div>
             <div className='background'>
                 <img className='img-1' src="https://img.freepik.com/premium-photo/koshari-egyptian-cuisine-white-plate_864588-11238.jpg" alt="طبق كشري" />
